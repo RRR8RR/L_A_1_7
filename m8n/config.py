922ce1d7164bbb6629eefd6f044fefdbc92b4914
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-12-04 18:24:46 +0300

    Update config.py

--- m8n/config.py
+++ m8n/config.py
@@ -10,9 +10,9 @@ load_dotenv()
 que = {}
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "BAB70I9bwA6g0NCWID3a_DefS11D4z8zo4CKkjq43ZKOLuUOeAgN6jIQvIygoCyqiqqF-gYuIoguooJB1SHyi27az1BxgQQooXUFDoh6jPKcZpIJlXvJ6NQp-lWCnMRy7iaZvZAnFr7KJ-n9ozY4BffbDW-qnbVRsHz8Io9tCmk5mm8-ueNzmr8Jk4yjYGm15AXHSLAims_tlULo_yWBFczMIzfoGv8ZydGar2qm2Sqd5V36nwdsM56GufFNH82UcOsbXejze3w6uexo1GDL-rZpuNnkaRLqAc6hZeh1Y_f-Qv5Kdhquy-qeHcNoXYRtuHHGkxTEh_YN_BpM7yEhIdP7AAAAAWIm-aQA")
-BOT_TOKEN = getenv("BOT_TOKEN", "6385191939:AAFBbpyzlAfzEnbMlU_UqUV60WD6xweSedM")
+BOT_TOKEN = getenv("BOT_TOKEN", "6385191939:AAEJmvdeSoCWAdk5Njf41_NQ2evdMGc026c")
 BOT_NAME = getenv("BOT_NAME", "bot")
-BOT_USERNAME = getenv("BOT_USERNAME", "Saiadjabot")
+BOT_USERNAME = getenv("BOT_USERNAME", "RU_NTBot")
 ASSID = int(getenv("ASSID", "6186856384"))
 ASSNAME = getenv("ASSNAME", "Kasijekg")
 ASSUSERNAME = getenv("ASSUSERNAME", "Kasijekg")
